0da226c53b41b5054ba7d625f36dfa6f98ae365b
Gean Gomes <geansg4@gmail.com>
2022-08-08 20:05:12 -0300

    chore: add responsive design on subscribe page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -35,22 +35,24 @@ export function Subscribe() {
   }
   return (
     <div className="min-h-screen bg-blur bg-no-repeat bg-cover flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px]">
-          <Logo />
+      <div className="w-full max-w-[1100px] flex flex-col items-center justify-between mt-20 mx-auto lg:flex-row">
+        <div className="max-w-[640px] text-center lg:text-left">
+          <div className="flex justify-center p-4 lg:justify-start">
+            <Logo />
+          </div>
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
+          <h1 className="mt-8 text-[2.5rem] leading-tight p-4">
             Construa uma{" "}
             <strong className="text-blue-500">aplicação completa</strong>, do
             zero, com <strong className="text-blue-500">React</strong>
           </h1>
-          <p className="mt-4 text-gray-200 leading-relaxed">
+          <p className="mt-4 text-gray-200 leading-relaxed p-2">
             Em apenas uma semana você vai dominar na prática uma das tecnologias
             mais utilizadas e com alta demanda para acessar as melhores
             oportunidades do mercado.
           </p>
         </div>
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="p-8 bg-gray-700 border border-gray-500 rounded mt-4 w-full sm:w-[40rem] lg:w-auto">
           <strong className="text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
